428a0371a14da54a3c0f7b67f4e64b136b91c4f5
planit <planit@tanjeffreyz.github.io>
2024-09-09 04:37:40 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,155 @@
-assignments = {};
+assignments = {
+  "CS 61A": [
+    {
+      "title": "Lab 0",
+      "course": "CS 61A",
+      "dueDate": "2024-09-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/843175/assignments/4835240/submissions/264479055",
+      "submitted": true
+    },
+    {
+      "title": "Homework 1",
+      "course": "CS 61A",
+      "dueDate": "2024-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/843175",
+      "submitted": false
+    },
+    {
+      "title": "Lab 1",
+      "course": "CS 61A",
+      "dueDate": "2024-09-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/843175/assignments/4856112/submissions/264509373",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "CS 61A",
+      "dueDate": "2024-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/843175",
+      "submitted": false
+    },
+    {
+      "title": "Hog Checkpoint",
+      "course": "CS 61A",
+      "dueDate": "2024-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/843175",
+      "submitted": false
+    },
+    {
+      "title": "Hog",
+      "course": "CS 61A",
+      "dueDate": "2024-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/843175",
+      "submitted": false
+    }
+  ],
+  "CS 61B": [
+    {
+      "title": "Homework 0A",
+      "course": "CS 61B",
+      "dueDate": "2024-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "Homework 0B",
+      "course": "CS 61B",
+      "dueDate": "2024-09-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "Project 0: 2048",
+      "course": "CS 61B",
+      "dueDate": "2024-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "Lab 01",
+      "course": "CS 61B",
+      "dueDate": "2024-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096/assignments/4835048/submissions/263506959",
+      "submitted": true
+    },
+    {
+      "title": "Lab 02",
+      "course": "CS 61B",
+      "dueDate": "2024-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "Homework 1",
+      "course": "CS 61B",
+      "dueDate": "2024-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096/assignments/4879194/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Project 1A: LinkedListDeque",
+      "course": "CS 61B",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "[UNGRADED] Project 1A: LinkedListDeque Test Coverage",
+      "course": "CS 61B",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    }
+  ],
+  "CS 70": [
+    {
+      "title": "Mini-Vitamin 0B",
+      "course": "CS 70",
+      "dueDate": "2024-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/4835238/submissions/263531760",
+      "submitted": true
+    },
+    {
+      "title": "Homework 0",
+      "course": "CS 70",
+      "dueDate": "2024-08-31T16:00:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/4835276/submissions/263578315",
+      "submitted": true
+    },
+    {
+      "title": "Mini-Vitamin 1A",
+      "course": "CS 70",
+      "dueDate": "2024-09-03T18:30:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/4835297/submissions/264130909",
+      "submitted": true
+    },
+    {
+      "title": "Mini-Vitamin 1B",
+      "course": "CS 70",
+      "dueDate": "2024-09-05T18:30:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/4835298/submissions/264769715",
+      "submitted": true
+    },
+    {
+      "title": "Homework 1",
+      "course": "CS 70",
+      "dueDate": "2024-09-07T16:00:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/4867304/submissions/265232599",
+      "submitted": true
+    },
+    {
+      "title": "Mini-Vitamin 2A",
+      "course": "CS 70",
+      "dueDate": "2024-09-10T19:30:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/4904817/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Mini-Vitamin 2B",
+      "course": "CS 70",
+      "dueDate": "2024-09-12T19:30:00",
+      "link": "https://www.gradescope.com/courses/836005/assignments/4904826/submissions/new",
+      "submitted": false
+    }
+  ]
+};
