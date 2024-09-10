09628740d50e2cf01f73594fe27c7b402ee8d089
planit <planit@tanjeffreyz.github.io>
2024-09-10 05:35:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -11,8 +11,8 @@ assignments = {
       "title": "Homework 1",
       "course": "CS 61A",
       "dueDate": "2024-09-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/843175",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/843175/assignments/4853921/submissions/265825880",
+      "submitted": true
     },
     {
       "title": "Lab 1",
@@ -90,8 +90,8 @@ assignments = {
       "title": "Homework 1",
       "course": "CS 61B",
       "dueDate": "2024-09-06T23:59:00",
-      "link": "https://www.gradescope.com/courses/845096/assignments/4879194/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/845096/assignments/4879194/submissions/265826689",
+      "submitted": true
     },
     {
       "title": "Project 1A: LinkedListDeque",
