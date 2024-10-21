62b11253e63124856caad899f5f6ac45c5cf418c
planit <planit@tanjeffreyz.github.io>
2024-10-21 23:33:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -323,8 +323,8 @@ assignments = {
       "title": "Project 2A: Ngrams",
       "course": "CS 61B",
       "dueDate": "2024-10-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/845096",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/845096/assignments/4928299/submissions/280428565",
+      "submitted": true
     },
     {
       "title": "Project 2B: Wordnet",
