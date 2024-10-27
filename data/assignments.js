1327458d057cc388f899feba3211ecff90a5c15b
planit <planit@tanjeffreyz.github.io>
2024-10-27 02:07:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -577,8 +577,8 @@ assignments = {
       "title": "Homework 8",
       "course": "CS 70",
       "dueDate": "2024-10-26T16:00:00",
-      "link": "https://www.gradescope.com/courses/836005",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/836005/assignments/5199704/submissions/282460214",
+      "submitted": true
     }
   ]
 };
