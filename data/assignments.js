0167f58722ebefc48338f95a4f1c110dff6c9d4b
planit <planit@tanjeffreyz.github.io>
2024-11-06 23:31:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -451,6 +451,20 @@ assignments = {
       "dueDate": "2024-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/845096",
       "submitted": false
+    },
+    {
+      "title": "[UNGRADED] Lab 09",
+      "course": "CS 61B",
+      "dueDate": "2024-04-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
+    },
+    {
+      "title": "[UNGRADED] Lab 10",
+      "course": "CS 61B",
+      "dueDate": "2024-04-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/845096",
+      "submitted": false
     }
   ],
   "CS 70": [
