c34403b7110798e1ae50c4c6950fefb6cfd85912
planit <planit@tanjeffreyz.github.io>
2024-12-07 05:37:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -554,8 +554,8 @@ assignments = {
       "title": "Project 2B: Design Document",
       "course": "CS 61B",
       "dueDate": "2024-12-07T23:59:00",
-      "link": "https://www.gradescope.com/courses/845096",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/845096/assignments/5199523/submissions/295478069",
+      "submitted": true
     },
     {
       "title": "[UNGRADED] Lab 09",
