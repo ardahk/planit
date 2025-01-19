ca415f6013f8ab97698002d88e88b7a809924935
planit <planit@tanjeffreyz.github.io>
2025-01-19 22:34:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,40 @@
 assignments = {
-  "CS 168": [],
+  "CS 168": [
+    {
+      "title": "Project 1A: Basic Traceroute",
+      "course": "CS 168",
+      "dueDate": "2025-02-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/943003",
+      "submitted": false
+    },
+    {
+      "title": "Project 1B: Traceroute Error Handling",
+      "course": "CS 168",
+      "dueDate": "2025-02-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/943003",
+      "submitted": false
+    },
+    {
+      "title": "Project 3A: Transport Checkpoint",
+      "course": "CS 168",
+      "dueDate": "2025-03-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/943003",
+      "submitted": false
+    },
+    {
+      "title": "Project 3B: Transport",
+      "course": "CS 168",
+      "dueDate": "2025-04-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/943003",
+      "submitted": false
+    },
+    {
+      "title": "Project 2: Routing",
+      "course": "CS 168",
+      "dueDate": "2025-02-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/943003",
+      "submitted": false
+    }
+  ],
   "CS 61C": []
 };
