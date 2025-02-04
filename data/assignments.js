cb92326fa4217d9c7aaa2c7aee2ae052a8a2b62d
planit <planit@tanjeffreyz.github.io>
2025-02-04 19:31:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -64,6 +64,13 @@ assignments = {
       "dueDate": "2025-02-06T23:59:00",
       "link": "https://www.gradescope.com/courses/952454",
       "submitted": false
+    },
+    {
+      "title": "Project 1",
+      "course": "CS 61C",
+      "dueDate": "2025-02-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/952454",
+      "submitted": false
     }
   ]
 };
