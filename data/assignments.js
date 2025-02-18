7d67ca84338d9434fd41de608119222edff410ee
planit <planit@tanjeffreyz.github.io>
2025-02-18 05:36:55 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -41,6 +41,13 @@ assignments = {
       "dueDate": "2025-02-24T23:59:00",
       "link": "https://www.gradescope.com/courses/943003/assignments/5719288/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "HW 2: Routing",
+      "course": "CS 168",
+      "dueDate": "2025-03-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/943003/assignments/5807210/submissions/new",
+      "submitted": false
     }
   ],
   "CS 61C": [
