ff6adaf668b467eab2f01bf33038508049adbe28
planit <planit@tanjeffreyz.github.io>
2025-03-22 05:36:49 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -18,8 +18,8 @@ assignments = {
       "title": "Project 3A: Transport Checkpoint",
       "course": "CS 168",
       "dueDate": "2025-03-21T23:59:00",
-      "link": "https://www.gradescope.com/courses/943003",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/943003/assignments/5631620/submissions/319180060",
+      "submitted": true
     },
     {
       "title": "Project 3B: Transport",
